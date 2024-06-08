a0abe3a0223d0f0aaf2e51e66ad140a09d0f65b0
Stefan Schramm <mail@stefanschramm.net>
2024-06-08 21:24:19 +0200

    Export more types

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
+import { type Environment, type NamespaceSource } from './core/Environment';
+import { Logger } from './core/Logger';
 import { QueryProcessingResultStatus, QueryProcessor } from './core/QueryProcessor';
 import { ObjectShortcutDatabase } from './core/database/ObjectShortcutDatabase';
+import { type Shortcut } from './core/database/Shortcut';
 import { GithubNamespaceSourceHandler } from './core/namespaces/GithubNamespaceSourceHandler';
 import { InPlaceNamespaceSourceHandler } from './core/namespaces/InPlaceNamespaceSourceHandler';
 import { NamespaceDispatcher } from './core/namespaces/NamespaceDispatcher';
 import { RemoteSingleJsonNamespaceSourceHandler } from './core/namespaces/RemoteSingleJsonNamespaceSourceHandler';
 import { UrlNamespaceSourceHandler } from './core/namespaces/UrlNamespaceSourceHandler';
-import { type NamespaceSource, type Environment } from './core/Environment';
-import { Logger } from './core/Logger';
 
 // Temporary API for usage in qxs-web. Not expected to be stable!
 
@@ -22,4 +23,4 @@ export default {
   Logger,
 };
 
-export type { Environment, NamespaceSource };
+export type { Environment, NamespaceSource, QueryProcessor, Shortcut };
